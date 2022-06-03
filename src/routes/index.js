e2b2c378b8d97cb4c3d3f780ddc204c08f07a044
Garduciz <94760419+Garduciz@users.noreply.github.com>
2022-06-03 17:03:54 -0300

    teste

---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import vendas from "../routes/vendasRoutes.js";
+import aluguel from "../routes/aluguelRoutes.js";
+import estoque from "../routes/estoqueRoutes.js";
+import fornecedor from "../routes/fornecedorRoutes.js";
+import cliente from "../routes/clienteRoutes.js";
+import livros from "../routes/livroRoutes.js";
+import funcionario from "../routes/funcionarioRoutes.js";
+
+
+const routes = (app) => {
+    app.route('/').get((req, res) => {
+        res.status(200).send({Mensagem: " Bem vindos a API da Livraria Macabéa 📚" })
+ });
+
+app.use(
+    express.json(),
+    vendas,
+    aluguel,
+    estoque,
+    fornecedor,
+    cliente,
+    livros,
+    funcionario
+    
+)
+}
+
+
+export default routes;
+
