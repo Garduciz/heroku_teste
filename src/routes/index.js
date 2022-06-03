c8f8726796bc453db7c73f8ced4fd8edb3389e65
Garduciz <94760419+Garduciz@users.noreply.github.com>
2022-06-03 17:52:55 -0300

    mensagem

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import vendas from "../routes/vendasRoutes.js";
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
-        res.status(200).send({Mensagem: " Bem vindos a API da Livraria Macabéa 📚" })
+        res.status(200).send({Mensagem: " Bem vindo ao nosso site Ifome 🍔" })
  });
 
 app.use(
